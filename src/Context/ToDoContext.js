fa43b082f9165dc721184bed2f105e8465218fc7
NOONECANSEE <anshusingla405@gmail.com>
2024-01-24 16:57:45 +0530

    API LOCAL STORAGE

---
+++ src/Context/ToDoContext.js
@@ -0,0 +1,15 @@
+import { createContext, useContext } from "react";
+
+export const ToDoContext = createContext({
+  todos: [{ id: 1, todo: "ToDo Msg", completed: false }],
+  addToDo: (todo) => {},
+  updateToDo: (id, todo) => {},
+  deleteToDo: (id) => {},
+  toggleComplete: (id) => {},
+});
+
+export const useToDo = () => {
+  return useContext(ToDoContext);
+};
+
+export const ToDoProvider = ToDoContext.Provider;
